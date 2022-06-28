d2903978cb0162a14fda38142918b104e22c649f
Mazen Coder <info@mazencoder.com>
2022-06-29 00:28:25 +0100

    init commit

--- feature/__brick__/main.dart
+++ feature/__brick__/main.dart
@@ -1,6 +1,7 @@
+import 'features/{{name.lowerCase()}}/presentation/pages/{{name.lowerCase()}}_page.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'core/injection/injection.dart';
 import 'core/langs/translation.dart';
