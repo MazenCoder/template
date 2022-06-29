60106c7311bc8130cd8f30195fcd40e1c8ef3231
Mazen Coder <info@mazencoder.com>
2022-06-29 03:48:01 +0100

    init commit

--- feature/__brick__/{{name.lowerCase()}}/domain/repositories/{{name.lowerCase()}}_repository.dart
+++
@@ -1,9 +0,0 @@
-import '../../data/models/{{name.lowerCase()}}_params.dart';
-import '../entities/{{name.lowerCase()}}_entity.dart';
-import '../../../../core/error/failures.dart';
-import 'package:dartz/dartz.dart';
-
-
-abstract class {{name.pascalCase()}}Repository {
-  Future<Either<Failure, {{name.pascalCase()}}Entity>> getConcrete{{name.pascalCase()}}({{name.pascalCase()}}Params params);
-}
