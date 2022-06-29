d9c924cb9502b792322087a2b2d1fac28454fd0c
Mazen Coder <info@mazencoder.com>
2022-06-29 01:24:04 +0100

    init commit

--- feature/__brick__/features/{{name.lowerCase()}}/data/repositories/{{name.lowerCase()}}_repository_impl.dart
+++ feature/__brick__/features/{{name.lowerCase()}}/data/repositories/{{name.lowerCase()}}_repository_impl.dart
@@ -1,18 +1,18 @@
-// import '../../domain/repositories/{{name.lowerCase()}}_repository.dart';
-// import '../datasources/{{name.lowerCase()}}_remote_data_source.dart';
-// import '../datasources/{{name.lowerCase()}}_local_data_source.dart';
-// import '../models/{{name.lowerCase()}}_params.dart';
-// import '../models/{{name.lowerCase()}}_model.dart';
-// import '../../../../core/usecases/constants.dart';
-// import '../../../../core/error/exceptions.dart';
-// import '../../../../core/error/failures.dart';
+import '../../domain/repositories/{{name.lowerCase()}}_repository.dart';
+import '../datasources/{{name.lowerCase()}}_remote_data_source.dart';
+import '../datasources/{{name.lowerCase()}}_local_data_source.dart';
+import '../models/{{name.lowerCase()}}_params.dart';
+import '../models/{{name.lowerCase()}}_model.dart';
+import '../../../../core/usecases/constants.dart';
+import '../../../../core/error/exceptions.dart';
+import '../../../../core/error/failures.dart';
 import 'package:injectable/injectable.dart';
 import 'package:dartz/dartz.dart';
 import 'package:meta/meta.dart';
 import 'package:get/get.dart';
 
 
-@LazySingleton(as: {{name.pascalCase()}Repository)
+@LazySingleton(as: {{name.pascalCase()}}Repository)
 class {{name.pascalCase()}}RepositoryImpl implements {{name.pascalCase()}}Repository {
   final {{name.pascalCase()}}RemoteDataSource remoteDataSource;
   final {{name.pascalCase()}}LocalDataSource localDataSource;
